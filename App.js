02e6a80eeb3d9b18d52dcaa31836f8a587569907
Shivam Gupta <shivamgupta2300@gmail.com>
2024-07-27 13:24:00 +0530

    few changes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {}, 'this is me');
 const root = ReactDOM.createRoot(document.getElementById("root"));
